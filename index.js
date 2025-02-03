50af66bf6253d5a6fa7548d02a9253bc6ab7ebc6
Anas Ardah <anas.m.ardah@gmail.com>
2025-02-03 21:13:30 +0300

    code refactor and improved coverage

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function validatePassword() {
         rule1.style.display = "block";
         rule2.style.display = "block";
         rule3.style.display = "block";
+        return;
     }
 
     validateFirstCharacter(firstCharacter);
@@ -24,21 +25,22 @@ function validatePassword() {
 }
 
 function validateFirstCharacter(firstCharacter) {
-    if (firstCharacter === firstCharacter.toUpperCase() && isNaN(firstCharacter)) {
-        rule2.style.display = "none";
-    } else {
+    //check by ascii
+    if(firstCharacter.charCodeAt(0) < 65 || firstCharacter.charCodeAt(0) > 90) {
         rule2.style.display = "block";
+        return;
     }
+    rule2.style.display = "none";
 }
 
 function validateLastCharacter(lastCharacter) {
-    console.log(typeof lastCharacter);
-    console.log(isNaN(lastCharacter));
-    if (!isNaN(lastCharacter)) {
-        rule3.style.display = "none";
-    } else {
+    //check by ascii
+    console.log(lastCharacter.charCodeAt(0));
+    if (lastCharacter.charCodeAt(0) < 48 || lastCharacter.charCodeAt(0) > 57) {
         rule3.style.display = "block";
+        return;
     }
+    rule3.style.display = "none";
 }
 
 function validateLength(passwordLength) {
