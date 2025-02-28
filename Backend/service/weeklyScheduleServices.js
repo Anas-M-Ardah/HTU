233442a6434b83d7404cacc96886bd9a636d3442
Anas Ardah <anas.m.ardah@gmail.com>
2025-02-28 23:53:28 +0300

    added ramadan schedule

--- Backend/service/weeklyScheduleServices.js
+++ Backend/service/weeklyScheduleServices.js
@@ -1,11 +1,19 @@
 const weeklyScheduleModel = require('../model/weeklySchedule');
 const { sequelize } = require('../config/db');
+const { Op } = require('sequelize');
 
 const WeeklyScheduleModel = weeklyScheduleModel(sequelize);
 
 const getWeeklyScheduleByCourseName = async (courseName) => {
     try {
-        const weeklySchedule = await WeeklyScheduleModel.findAll({ where: { courseName } });
+        const weeklySchedule = await WeeklyScheduleModel.findAll({
+            where: {
+                courseName,
+                createdAt: {
+                    [Op.lt]: new Date('2024-02-27')
+                }
+            }
+        });
         return weeklySchedule;
     } catch (error) {
         console.error('Error occurred while retrieving weekly schedule by course name:', error);
@@ -17,8 +25,8 @@ const getWeeklyScheduleByCourseNameRamadan = async (courseName) => {
     try {
         const weeklySchedule = await WeeklySchedule.findAll({
             where: {
-              courseName,
-              createdAt: new Date('2025-02-28')
+                courseName,
+                createdAt: new Date('2025-02-28')
             }
         });
         return weeklySchedule;
